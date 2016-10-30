e1484ecfc7c274af518f41335e53c958076e1cc4
Kevin <tig3r86@gmail.com>
2016-10-30 14:00:09 +1100

    debug

--- controller/botkit.js
+++ controller/botkit.js
@@ -246,7 +246,7 @@ const tellPartTwoLifeStory = (bot, message) => {
   bot.startTyping(message, () => {})
   bot.reply(message, "One day, in an act of kindess, Kevin's grandpa who was living in Australia applied to sponsor Kevin's and his family to migrate to Australia", () => {
     bot.replyWithTyping(message, "Because of that, Kevin was able to grow up in Australia. A land of the free and boundless opportunity. However, there was a difficult period where language barrier and cultural differences threaten to derails Kevin's plan to become the first in his family to graduate from university", () => {
-      let text = "What happen next?"
+      let text = ""
       let quickReplies = [
         {
           type: "text",
