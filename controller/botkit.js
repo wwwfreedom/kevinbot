741bd948ff4caa02b9e1f45bec0ed20771b2d678
Kevin <tig3r86@gmail.com>
2016-10-31 13:48:00 +1100

    test

--- controller/botkit.js
+++ controller/botkit.js
@@ -264,14 +264,13 @@ const tellPartOneLifeStory = (bot, message) => {
         convo.say("He's had an interesting but complicated story.")
         convo.say(readyImg)
       })
-    }, 1000)
-
-    bot.startTyping(message, () => {})
-    setTimeout(() => {
-      bot.stopTyping(message, () => {
-        convo.say("He grew up on North side of Adelaide - mostly in Salisbury Downs in this house:")
-        convo.say(houseImg)
-      })
+      bot.startTyping(message, () => {})
+      setTimeout(() => {
+        bot.stopTyping(message, () => {
+          convo.say("He grew up on North side of Adelaide - mostly in Salisbury Downs in this house:")
+          convo.say(houseImg)
+        })
+      }, 1000)
     }, 1000)
   })
   /* bot.startTyping(message, () => {})
