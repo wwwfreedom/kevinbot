ff7d74971e5b10e4f1ac55cdf51d76df4d864c7c
Kevin <tig3r86@gmail.com>
2016-11-09 12:45:14 +1100

    debug user name in welcome prompt

--- controller/botkit.js
+++ controller/botkit.js
@@ -48,7 +48,8 @@ const sendWelcomePromt = (bot, message) => {
     qs: {access_token:process.env.page_token},
     method: 'GET',
   }, function(error, response, body) {
-    console.log(response, body)
+    console.log(body)
+    console.log(body.first_name)
     if (error) {
       console.log('Error sending messages: ', error)
     } else if (response.body.error) {
