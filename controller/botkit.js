56f1ff456926c2d98eaeda34d9bc301c7a06f4b3
Kevin <tig3r86@gmail.com>
2016-11-09 13:10:16 +1100

    debug name

--- controller/botkit.js
+++ controller/botkit.js
@@ -48,9 +48,8 @@ const sendWelcomePromt = (bot, message) => {
     qs: {access_token:process.env.page_token},
     method: 'GET',
   }, function(error, response, body) {
-    console.log(body, "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-    console.log(body.toString())
     console.log(JSON.parse(body))
+    console.log(JSON.parse(body.toString()), "####################")
     if (error) {
       console.log('Error sending messages: ', error)
     } else if (response.body.error) {
